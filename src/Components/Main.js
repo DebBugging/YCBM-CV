2d8897d73f4f65feda365ba72d65e4876e1f63de
Deborah Castiglioni <debjgorton@gmail.com>
2018-11-16 03:48:30 -0600

    Finish layout

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -11,19 +11,21 @@ class Main extends Component {
           websites fast and easy.
         </h3>
         <a role="link" title="Click to Schedule" href="http://www.lifecodeson.com/contact-me/" target="_blank">
-        <img src={schedule} alt="Schedule an interview."></img>
+          <img src={schedule} alt="Schedule an interview."></img>
         </a>
 
-        <div className="about-me">
+<div className="about-me">
+<h4>About Me</h4>
+<p>Wife, mother, and self-taught front-end web developer. Self-taught means that I learned everything from scratch and had the tenacity to keep going without any incentive pushing me forward except for my own dreams and passion. It means that I reached out and found a supportive community on my own and that I learned how to work alongside others from far away. I am dedicated. I have taken the time to research topics that I'm not familiar with and I have learned to ask questions and give answers. I am going to use the skills that I have to keep advancing.</p>
 
-          <p>Wife, mother, and self-taught front-end web developer. Self-taught means that I learned everything from scratch and had the tenacity to keep going without any incentive pushing me forward except for my own dreams and passion. It means that I reached out and found a supportive community on my own and that I learned how to work alongside others from far away. I am dedicated. I have taken the time to research topics that I'm not familiar with and I have learned to ask questions and give answers. I am going to use the skills that I have to keep advancing.</p>
+<p>I am currently living in Costa Rica and am fully bilingual (English/Spanish). I really love the values of this company and know that I would make a great fit. Please look around and click on my CV's.</p> 
 
-          <p>I am currently living in Costa Rica and am fully bilingual (English/Spanish). I really love the values of this company and know that I would make a great fit. Please look around and click on my CV's.</p> 
+<p>I look forward to hearing back soon. Have a great week!</p>
+<p>Saludos! -Deborah Castiglioni</p>
 
-          <p>I look forward to hearing back soon. Have a great week!</p>
+</div>
 
-        </div>
-  </div>
+      </div>
       );
   }
 }
