1b70362bebe4c2dea4e4aee291aaa6b351520920
Deborah Castiglioni <debjgorton@gmail.com>
2018-11-15 18:19:41 -0600

    Add target blank to a tag

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,10 +4,19 @@ class Header extends Component {
   render() {
     return (
       <div className="navbar">
-        <img />
-        <button>CV English</button>
-        <button>CV Español</button>
-        <button>Portfolio-Life Codes On</button>
+        <a
+          class="navbar-brand"
+          href="http://www.lifecodeson.com/who-is-debs/"
+          target="_blank"
+          title="YouCanBook.Deborah"
+        >
+          <h1>YouCanBook.Deborah</h1>
+        </a>
+        <div className="navbar-buttons">
+          <button>CV English</button>
+          <button>CV Español</button>
+          <button>Portfolio-Life Codes On</button>
+        </div>
       </div>
     );
   }
